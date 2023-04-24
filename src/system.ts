2b6511d505387a41f0ea4d02b621bb5039393047
mxnf131 <128838337+mxnf131@users.noreply.github.com>
2023-04-24 18:45:54 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -11,7 +11,7 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `Powered by OpenAI Vercel
-- 网站经常被墙，请保存永久网址：[mxnf.tk ](http://gpt.mxnf.one)
+- 网站经常被墙，请保存永久网址：[mxnf.tk ](http://gpt1.mxnf.one)
 - 本网站仅作为项目演示，不提供长期服务，长期使用请 [联系站长](http://img.iswl.tk/123/123.jpg)，获取独立账号+秘钥+高级网站!。
 - 个位数 [搭建同款](http://img.iswl.tk/123/Untitled-1.html) 和 [进交流群](https://jq.qq.com/?_wv=1027&k=NE2LYhyf) 
 - [Pro版已发布，功能更多，](http://gpt4.iswl.tk)[点我体验](http://gpt6.iswl.tk),开头输入 [[空格]] 跳出更多预设。`
