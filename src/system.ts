50b9617bf04133e21a8fe4579fb3d99e4d810126
mxnf131 <128838337+mxnf131@users.noreply.github.com>
2023-06-14 23:45:27 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -7,7 +7,7 @@ export const defaultSetting = {
   openaiAPITemperature: 60,
   password: "",
   systemRule: "",
-  model: "gpt-3.5-turbo" as Model
+  model: "gpt-3.5-turbo-16k" as Model
 }
 
 export const defaultMessage = `Powered by OpenAI Vercel
@@ -21,9 +21,9 @@ export type Setting = typeof defaultSetting
 export const defaultResetContinuousDialogue = false
 
 export const defaultMaxInputTokens: Record<Model, number> = {
-  "gpt-3.5-turbo": 3072,
+  "gpt-3.5-turbo": 16000,
   "gpt-4": 6144,
   "gpt-4-32k": 24576
 }
 
-export const defaultModel: Model = "gpt-3.5-turbo"
+export const defaultModel: Model = "gpt-3.5-turbo-16k"
